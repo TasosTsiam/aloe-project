c3249db6249ebce05bae36f059b09de95fe3f681
TasosTsiam <mfs170lvl@hotmail.com>
2023-06-29 15:55:25 +0300

    Applied appropriate paths for components

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -6,7 +6,7 @@ function Nav() {
     return (
         <nav className="navbar navbar-expand-lg">
             <div className="container-fluid d-flex justify-content-center">
-                <Link className="navbar-brand col-6 col-lg-4" to="/main">
+                <Link className="navbar-brand col-6 col-lg-4" to="/homepage">
                     <img src={AloeLogo} alt="Aloe Logo"/>
                 </Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
@@ -14,10 +14,10 @@ function Nav() {
                 </button>
                 <div className="collapse navbar-collapse col-6 col-lg-8" id="navbarTogglerDemo02">
                     <ul className="navbar-nav mb-2 mb-lg-0 text-nowrap">
-                        <li className="nav-item"><Link className="nav-link" aria-current="page" to="/main">Home</Link></li>
-                        <li className="nav-item"><Link className="nav-link" aria-current="page" to="/main">Order Online</Link></li>
-                        <li className="nav-item"><Link className="nav-link" aria-current="page" to="/main">About</Link></li>
-                        <li className="nav-item"><Link className="nav-link" aria-current="page" to="/main">Login</Link></li>
+                        <li className="nav-item"><Link className="nav-link" aria-current="page" to="/homepage">Home</Link></li>
+                        <li className="nav-item"><Link className="nav-link" aria-current="page" to="/homepage">About</Link></li>
+                        <li className="nav-item"><Link className="nav-link" aria-current="page" to="/order">Order Online</Link></li>
+                        <li className="nav-item"><Link className="nav-link" aria-current="page" to="/login">Login</Link></li>
                     </ul>
                 </div>
             </div>
