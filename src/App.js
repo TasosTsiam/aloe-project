0b5c51f992064ac1d45b612f4c4c9f7114fa8a8d
TasosTsiam <mfs170lvl@hotmail.com>
2023-06-29 21:00:16 +0300

    Worked on Nav.js and the style.scss for it

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
         <Router>
             <div className="container-fluid d-flex flex-column h-100">
                 <div className="row justify-content-between">
-                    <div className="col-12">
+                    <div className="col-6">
                         <Nav />
                     </div>
                 </div>
