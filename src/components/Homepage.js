baf35c0dd80a6c030d35c8d1f164da8b717e1ebf
TasosTsiam <mfs170lvl@hotmail.com>
2023-07-07 16:11:03 +0300

    Fantastic progress, learning new stuff about SVG and transitions

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -13,14 +13,19 @@ function Homepage() {
 
     const [backgroundImage, setBackgroundImage] = useState(aloe1);
 
+    const [boxShadow, setBoxShadow] = useState('');
+
     function hover1() {
         setBackgroundImage(aloe1);
+        setBoxShadow('50px 60px 120px 20px white');
     }
     function hover2() {
         setBackgroundImage(aloe2);
+        setBoxShadow('-50px 50px 120px 20px #FFE000');
     }
     function hover3() {
         setBackgroundImage(aloe3);
+        setBoxShadow('0px 0px 150px 30px #00FC91');
     }
 
     return (
@@ -66,7 +71,13 @@ function Homepage() {
                             </div>
                         </div>
                         <div className="s1-right-div d-flex justify-content-center col-lg-6 col-md-6 col-sm-6 col-12">
-                            <div className="d-flex justify-content-center gradient-img-container" style={{"background-image": `url(${backgroundImage})`}}>
+                            <div className="d-flex justify-content-center gradient-img-container"
+                                style={
+                                    {"background-image": `url(${backgroundImage})`,
+                                    boxShadow: boxShadow}
+
+                                }
+                            >
                                 <div className="gradient-image-1 d-flex justify-content-center"
                                     style={
                                         backgroundImage === aloe1 ? {
@@ -101,7 +112,13 @@ function Homepage() {
                                             backgroundImage === aloe3 ? {height: '2rem'} : {}
                                         }
                                     >
-                                        <path d="M96 0C82.7 0 72 10.7 72 24s10.7 24 24 24c4.4 0 8 3.6 8 8v64.9c0 12.2-7.2 23.1-17.2 30.1C53.7 174.1 32 212.5 32 256V448c0 35.3 28.7 64 64 64H224c35.3 0 64-28.7 64-64V256c0-43.5-21.7-81.9-54.8-105c-10-7-17.2-17.9-17.2-30.1V56c0-4.4 3.6-8 8-8c13.3 0 24-10.7 24-24s-10.7-24-24-24l-8 0 0 0 0 0H104l0 0 0 0L96 0zm64 382c-26.5 0-48-20.1-48-45c0-16.8 22.1-48.1 36.3-66.4c6-7.8 17.5-7.8 23.5 0C185.9 288.9 208 320.2 208 337c0 24.9-21.5 45-48 45z"/>
+                                        <defs>
+                                            <linearGradient id="fillGradient1" x1="0" y1="1" x2="0" y2="0">
+                                                <stop offset="0%" stop-color="#DEFF5C" />
+                                                <stop offset="100%" stop-color="white" />
+                                            </linearGradient>
+                                        </defs>
+                                        <path fill="url(#fillGradient1)" d="M96 0C82.7 0 72 10.7 72 24s10.7 24 24 24c4.4 0 8 3.6 8 8v64.9c0 12.2-7.2 23.1-17.2 30.1C53.7 174.1 32 212.5 32 256V448c0 35.3 28.7 64 64 64H224c35.3 0 64-28.7 64-64V256c0-43.5-21.7-81.9-54.8-105c-10-7-17.2-17.9-17.2-30.1V56c0-4.4 3.6-8 8-8c13.3 0 24-10.7 24-24s-10.7-24-24-24l-8 0 0 0 0 0H104l0 0 0 0L96 0zm64 382c-26.5 0-48-20.1-48-45c0-16.8 22.1-48.1 36.3-66.4c6-7.8 17.5-7.8 23.5 0C185.9 288.9 208 320.2 208 337c0 24.9-21.5 45-48 45z"/>
                                     </svg>
                                 </div>
                                 <div className="gradient-image-2 d-flex justify-content-center"
@@ -138,7 +155,13 @@ function Homepage() {
                                             backgroundImage === aloe3 ? {height: '5.5rem'} : {}
                                         }
                                     >
-                                        <path d="M96 0C82.7 0 72 10.7 72 24s10.7 24 24 24c4.4 0 8 3.6 8 8v64.9c0 12.2-7.2 23.1-17.2 30.1C53.7 174.1 32 212.5 32 256V448c0 35.3 28.7 64 64 64H224c35.3 0 64-28.7 64-64V256c0-43.5-21.7-81.9-54.8-105c-10-7-17.2-17.9-17.2-30.1V56c0-4.4 3.6-8 8-8c13.3 0 24-10.7 24-24s-10.7-24-24-24l-8 0 0 0 0 0H104l0 0 0 0L96 0zm64 382c-26.5 0-48-20.1-48-45c0-16.8 22.1-48.1 36.3-66.4c6-7.8 17.5-7.8 23.5 0C185.9 288.9 208 320.2 208 337c0 24.9-21.5 45-48 45z"/>
+                                        <defs>
+                                            <linearGradient id="fillGradient2" x1="0" y1="1" x2="0" y2="0">
+                                                <stop offset="0%" stop-color="#990012" />
+                                                <stop offset="100%" stop-color="white" />
+                                            </linearGradient>
+                                        </defs>
+                                        <path fill="url(#fillGradient2)" d="M96 0C82.7 0 72 10.7 72 24s10.7 24 24 24c4.4 0 8 3.6 8 8v64.9c0 12.2-7.2 23.1-17.2 30.1C53.7 174.1 32 212.5 32 256V448c0 35.3 28.7 64 64 64H224c35.3 0 64-28.7 64-64V256c0-43.5-21.7-81.9-54.8-105c-10-7-17.2-17.9-17.2-30.1V56c0-4.4 3.6-8 8-8c13.3 0 24-10.7 24-24s-10.7-24-24-24l-8 0 0 0 0 0H104l0 0 0 0L96 0zm64 382c-26.5 0-48-20.1-48-45c0-16.8 22.1-48.1 36.3-66.4c6-7.8 17.5-7.8 23.5 0C185.9 288.9 208 320.2 208 337c0 24.9-21.5 45-48 45z"/>
                                     </svg>
                                 </div>
                                 <div className="gradient-image-3 d-flex justify-content-center"
@@ -175,7 +198,13 @@ function Homepage() {
                                             backgroundImage === aloe3 ? {height: '3rem'} : {}
                                         }
                                     >
-                                        <path d="M96 0C82.7 0 72 10.7 72 24s10.7 24 24 24c4.4 0 8 3.6 8 8v64.9c0 12.2-7.2 23.1-17.2 30.1C53.7 174.1 32 212.5 32 256V448c0 35.3 28.7 64 64 64H224c35.3 0 64-28.7 64-64V256c0-43.5-21.7-81.9-54.8-105c-10-7-17.2-17.9-17.2-30.1V56c0-4.4 3.6-8 8-8c13.3 0 24-10.7 24-24s-10.7-24-24-24l-8 0 0 0 0 0H104l0 0 0 0L96 0zm64 382c-26.5 0-48-20.1-48-45c0-16.8 22.1-48.1 36.3-66.4c6-7.8 17.5-7.8 23.5 0C185.9 288.9 208 320.2 208 337c0 24.9-21.5 45-48 45z"/>
+                                        <defs>
+                                            <linearGradient id="fillGradient3" x1="0" y1="1" x2="0" y2="0">
+                                                <stop offset="0%" stop-color="#EBA937" />
+                                                <stop offset="100%" stop-color="white" />
+                                            </linearGradient>
+                                        </defs>
+                                        <path fill="url(#fillGradient3)" d="M96 0C82.7 0 72 10.7 72 24s10.7 24 24 24c4.4 0 8 3.6 8 8v64.9c0 12.2-7.2 23.1-17.2 30.1C53.7 174.1 32 212.5 32 256V448c0 35.3 28.7 64 64 64H224c35.3 0 64-28.7 64-64V256c0-43.5-21.7-81.9-54.8-105c-10-7-17.2-17.9-17.2-30.1V56c0-4.4 3.6-8 8-8c13.3 0 24-10.7 24-24s-10.7-24-24-24l-8 0 0 0 0 0H104l0 0 0 0L96 0zm64 382c-26.5 0-48-20.1-48-45c0-16.8 22.1-48.1 36.3-66.4c6-7.8 17.5-7.8 23.5 0C185.9 288.9 208 320.2 208 337c0 24.9-21.5 45-48 45z"/>
                                     </svg>
                                 </div>
                             </div>
