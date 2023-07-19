2c2a60c4c046e626df3bf897912d07801312e6e7
TasosTsiam <mfs170lvl@hotmail.com>
2023-07-19 17:07:34 +0300

    Modified products layout and style.

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,117 +1,73 @@
 import React from "react";
 import aloeBottle from '../images/aloe-bottle.jpg';
-import aloeLeaves from '../images/aloe-leaves.jpg';
-import aloeFlowerpot from '../images/aloe-flowerpot.jpg';
 
 
 function Products () {
 
     return (
-        <div className="container products-container col-12 col-md-6 col-lg-6 col-xl-6 col-xxl-6" style={{ margin: "20rem 0" }}>
-            <div className="row">
-                <div id="carouselExampleIndicators" className="carousel slide w-75">
-                    <div className="carousel-indicators">
-                        <button
-                            type="button"
-                            data-bs-target="#carouselExampleIndicators"
-                            data-bs-slide-to="0"
-                            className="active"
-                            aria-current="true"
-                            aria-label="Slide 1"
-                        ></button>
-                        <button
-                            type="button"
-                            data-bs-target="#carouselExampleIndicators"
-                            data-bs-slide-to="1"
-                            aria-label="Slide 2"
-                        ></button>
-                        <button
-                            type="button"
-                            data-bs-target="#carouselExampleIndicators"
-                            data-bs-slide-to="2"
-                            aria-label="Slide 3"
-                        ></button>
-                    </div>
-                    <div className="carousel-inner">
-                        <div className="carousel-item active">
-                            <div className="d-flex card-container justify-content-center" style={{ height: "600px" }}>
-                                <div className="card d-block w-50" style={{ objectFit: "cover" }}>
-                                    <img src={aloeBottle} alt="Card" className="card-img-top"/>
-                                    <div className="card-body d-flex flex-column justify-content-between">
-                                        <div className="row">
-                                            <h5 className="col-6 card-title text-nowrap">Bottles</h5>
-                                            <p className="col-6 card_price text-nowrap text-end">$ 3.00</p>
-                                        </div>
-                                        <p className="card-text">
-                                            Ready-for-use gel of highest quality.
-                                        </p>
-                                        <button className="btn btn-primary col-12">
-                                            Order
-                                            <span className="delivery-icon"> &#x1F69A;</span>
-                                        </button>
-                                    </div>
-                                </div>
+        <div className="container py-3 px-3 products-container col-12 col-md-6 col-lg-6 col-xl-6 col-xxl-6" style={{ margin: "20rem 0" }}>
+            <div className="line-effect-wrapper py-5 px-5">
+                <div className="row">
+                    <div className="col-12 col-md-6 col-lg-6 col-xl-4 col-xxl-4">
+                        <div className="card h-100">
+                            <div className="image-container">
+                                <img src={aloeBottle} alt="Card" className="card-img-top" style={{ borderRadius: "16px"}}/>
                             </div>
-                        </div>
-                        <div className="carousel-item">
-                            <div className="d-flex card-container justify-content-center" style={{ height: "600px" }}>
-                                <div className="card d-block w-50" style={{ objectFit: "cover" }}>
-                                    <img src={aloeLeaves} alt="Card" className="card-img-top"/>
-                                    <div className="card-body d-flex flex-column justify-content-between">
-                                        <div className="row">
-                                            <h5 className="col-6 card-title text-nowrap">Leaves</h5>
-                                            <p className="col-6 card_price text-nowrap text-end">$ 2.00</p>
-                                        </div>
-                                        <p className="card-text">
-                                            Freshly cut leaves to make your own aloe vera gel.
-                                        </p>
-                                        <button className="btn btn-primary col-12">
-                                            Order
-                                            <span className="delivery-icon"> &#x1F69A;</span>
-                                        </button>
-                                    </div>
+                            <div className="card-body d-flex flex-column justify-content-between">
+                                <div className="row">
+                                    <h5 className="col-6 card-title text-nowrap">Bottles</h5>
+                                    <p className="col-6 card_price text-nowrap text-end">$ 3.00</p>
                                 </div>
-                            </div>
-                        </div>
-                        <div className="carousel-item">
-                            <div className="d-flex card-container justify-content-center" style={{ height: "600px" }}>
-                                <div className="card d-block w-50" style={{ objectFit: "cover" }}>
-                                    <img src={aloeFlowerpot} alt="Card" className="card-img-top"/>
-                                    <div className="card-body d-flex flex-column justify-content-between">
-                                        <div className="row">
-                                            <h5 className="col-6 card-title text-nowrap">Plants</h5>
-                                            <p className="col-6 card_price text-nowrap text-end">$ 5.00</p>
-                                        </div>
-                                        <p className="card-text">
-                                            Aloe bottles
-                                        </p>
-                                        <button className="btn btn-primary col-12">
-                                            Order
-                                            <span className="delivery-icon"> &#x1F69A;</span>
-                                        </button>
-                                    </div>
-                                </div>
+                                <p className="card-text">
+                                    Ready-for-use gel of highest quality.
+                                </p>
+                                <button className="btn btn-primary col-12">
+                                    Order
+                                    <span className="delivery-icon"> &#x1F69A;</span>
+                                </button>
                             </div>
                         </div>
                     </div>
-                    <button
-                        className="carousel-control-prev"
-                        type="button"
-                        data-bs-target="#carouselExampleIndicators"
-                        data-bs-slide="prev"
-                    >
-                        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-                        <span className="visually-hidden">Previous</span>
-                    </button>
-                    <button
-                        className="carousel-control-next"
-                        type="button"
-                        data-bs-target="#carouselExampleIndicators"
-                        data-bs-slide="next"
-                    >
-                        <span className="carousel-control-next-icon" aria-hidden="true"></span>
-                        <span className="visually-hidden">Next</span>
-                    </button>
+                    <div className="col-12 col-md-6 col-lg-6 col-xl-4 col-xxl-4">
+                        <div className="card h-100">
+                            <div className="image-container">
+                                <img src={aloeBottle} alt="Card" className="card-img-top" style={{ borderRadius: "16px"}}/>
+                            </div>
+                            <div className="card-body d-flex flex-column justify-content-between">
+                                <div className="row">
+                                    <h5 className="col-6 card-title text-nowrap">Leaves</h5>
+                                    <p className="col-6 card_price text-nowrap text-end">$ 2.00</p>
+                                </div>
+                                <p className="card-text">
+                                    Freshly cut leaves to make your own aloe vera gel.
+                                </p>
+                                <button className="btn btn-primary col-12">
+                                    Order
+                                    <span className="delivery-icon"> &#x1F69A;</span>
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-12 col-md-6 col-lg-6 col-xl-4 col-xxl-4">
+                        <div className="card h-100">
+                            <div className="image-container">
+                                <img src={aloeBottle} alt="Card" className="card-img-top" style={{ borderRadius: "16px"}}/>
+                            </div>
+                            <div className="card-body d-flex flex-column justify-content-between">
+                                <div className="row">
+                                    <h5 className="col-6 card-title text-nowrap">Plants</h5>
+                                    <p className="col-6 card_price text-nowrap text-end">$ 5.00</p>
+                                </div>
+                                <p className="card-text">
+                                    Plant your own Aloe Vera!
+                                </p>
+                                <button className="btn btn-primary col-12">
+                                    Order
+                                    <span className="delivery-icon"> &#x1F69A;</span>
+                                </button>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
